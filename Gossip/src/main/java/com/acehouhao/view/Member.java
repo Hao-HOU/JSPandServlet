a586418abd9d3c0b755a04fd4cb3662b197b12c9
Hao HOU <houhao118@163.com>
2017-07-10 13:00:28 +0800

    Gossip

---
+++ Gossip/src/main/java/com/acehouhao/view/Member.java
@@ -0,0 +1,31 @@
+package com.acehouhao.view;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 会员页面
+ * Created by Hao HOU on 2017/7/10.
+ */
+@WebServlet("/member.view")
+public class Member extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>登录成功</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>会员 " + req.getParameter("username") + " 你好</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
